4095aaafa7c9f158b3a2950b7f382a0127c1c728
haavarhu <havard.hunshamar@ntnu.no>
2018-09-05 10:38:23 +0200

    Added header component for the menu #14

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import MenuHeader from '../components/MenuHeader';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className="App-title">Velkommen til prosjekt 2</h1>
+        <MenuHeader menuHeaderTitle="abc" />
       </div>
     );
   }
