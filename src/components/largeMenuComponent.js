9120cc359d447c8ac858380481817146da3c190b
sondre <sondrewold1@gmail.com>
2018-09-10 11:56:18 +0200

    linting #32

--- src/components/largeMenuComponent.js
+++ src/components/largeMenuComponent.js
@@ -6,18 +6,18 @@ export class LargeMenuComponent extends Component {
     render () {
         return (
             <div className='largeMenuComponent'>
-            <MenuHeader menuHeaderTitle="Media group 1"/>
-            <RadioButton text="Option 1"/>
-            <RadioButton text="Option 1"/>
-            <RadioButton text="Option 1"/>
-            <MenuHeader menuHeaderTitle="Media group 2"/>
-            <RadioButton text="Option 1"/>
-            <RadioButton text="Option 1"/>
-            <RadioButton text="Option 1"/>
-            <MenuHeader menuHeaderTitle="Media group 3"/>
-            <RadioButton text="Option 1"/>
-            <RadioButton text="Option 1"/>
-            <RadioButton text="Option 1"/>
+                <MenuHeader menuHeaderTitle="Media group 1"/>
+                <RadioButton text="Option 1"/>
+                <RadioButton text="Option 1"/>
+                <RadioButton text="Option 1"/>
+                <MenuHeader menuHeaderTitle="Media group 2"/>
+                <RadioButton text="Option 1"/>
+                <RadioButton text="Option 1"/>
+                <RadioButton text="Option 1"/>
+                <MenuHeader menuHeaderTitle="Media group 3"/>
+                <RadioButton text="Option 1"/>
+                <RadioButton text="Option 1"/>
+                <RadioButton text="Option 1"/>
             </div>
         )
     }
