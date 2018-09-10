2646ee34d1df5843c8e3f92e329bf3d0760de038
sondre <sondrewold1@gmail.com>
2018-09-10 10:34:42 +0200

    Made picture frame component #19

---
+++ src/components/pictureFrame.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class PictureFrame extends Component {
+    render () {
+        return (
+            <div className="pictureFrame">
+            </div>
+        )
+    }
+}
+
