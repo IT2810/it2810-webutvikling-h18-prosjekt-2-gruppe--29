8cc371e2ecdf0ffa99a3f13177eab094811a0ae6
sondre <sondrewold1@gmail.com>
2018-09-10 11:20:18 +0200

    added menu component for large screens #32

---
+++ src/components/contentContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { LargeMenuComponent } from './largeMenuComponent.js';
+import { ExContainer } from './exContainer.js';
+
+
+export class ContentContainer extends Component {
+    render () {
+        return (
+            <div className='contentContainer'>
+            <ExContainer></ExContainer>
+            <LargeMenuComponent></LargeMenuComponent>
+            </div>
+        )
+    }
+}
+
---
+++ src/components/largeMenuComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { RadioButton } from './radioButton.js';
+import { MenuHeader } from './MenuHeader.js';
+
+export class LargeMenuComponent extends Component {
+    render () {
+        return (
+            <div className='largeMenuComponent'>
+            <MenuHeader menuHeaderTitle="Media group 1"/>
+            <RadioButton text="Option 1"/>
+            <RadioButton text="Option 1"/>
+            <RadioButton text="Option 1"/>
+            <MenuHeader menuHeaderTitle="Media group 2"/>
+            <RadioButton text="Option 1"/>
+            <RadioButton text="Option 1"/>
+            <RadioButton text="Option 1"/>
+            <MenuHeader menuHeaderTitle="Media group 3"/>
+            <RadioButton text="Option 1"/>
+            <RadioButton text="Option 1"/>
+            <RadioButton text="Option 1"/>
+            </div>
+        )
+    }
+}
+
