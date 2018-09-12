84128e48305e0ef990fc6d9c6791827711ae4f71
sondre <sondrewold1@gmail.com>
2018-09-12 14:38:20 +0200

    Added Music component #37

--- src/components/exContainer.js
+++ src/components/exContainer.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { DynamicPictureContainer } from './dynamicPictureContainer';
 import { TabContainer } from './tabContainer';
+import dogBark from '../audio/animals/dogBark.mp3'
 
 export class ExContainer extends Component {
     render () {
         return (
             <div className="exContainer">
                 <TabContainer />
-                <DynamicPictureContainer textName="joke1" imgName="Baloo"/>
+                <DynamicPictureContainer textName="joke1" imgName="Baloo" audioName={dogBark}/>
             </div>
         )
     }
