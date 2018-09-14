3fd464d836d2097acdb048e79a0b106db503a994
haavarhu <havard.hunshamar@ntnu.no>
2018-09-14 15:07:31 +0200

    Created menu with dynamically rendered react components, animation and correct style #43

--- src/components/radioButton.js
+++ src/components/radioButton.js
@@ -4,25 +4,35 @@ export class RadioButton extends Component {
 
   render() {
 
+    this.defaultProps = {
+      buttonAfter : false,
+    }
+
     return (
-      <div className="radioButtonLine">
+      <div className="radioButton">
         <form>
-          <div>
+          <div className="radioButtonLine">
             <label>
-              <input className="radioButton" type="radio" name="radioButtonGroup" value={this.props.opt1} />
-              {this.props.opt1}
+              {this.props.buttonAfter && this.props.opt1}
+              <input type="radio" name="radioButtonGroup" value={this.props.opt1} />
+              {this.props.buttonAfter && <span className="checkmark"><span className="mark"></span></span> }
+              {!this.props.buttonAfter && this.props.opt1}
             </label>
           </div>
-          <div>
+          <div className="radioButtonLine">
             <label>
-              <input className="radioButton" type="radio" name="radioButtonGroup" value={this.props.opt2} />
-              {this.props.opt2}
+              {this.props.buttonAfter && this.props.opt2 }
+              <input type="radio" name="radioButtonGroup" value={this.props.opt2} />
+              {this.props.buttonAfter && <span className="checkmark"><span className="mark"></span></span> }
+              {!this.props.buttonAfter && this.props.opt2 }
             </label>
           </div>
-          <div>
+          <div className="radioButtonLine">
             <label>
-              <input className="radioButton" type="radio" name="radioButtonGroup" value={this.props.opt3} />
-              {this.props.opt3}
+              {this.props.buttonAfter && this.props.opt3}
+              <input type="radio" name="radioButtonGroup" value={this.props.opt3} />
+              {this.props.buttonAfter && <span className="checkmark"><span className="mark"></span></span> }
+              {!this.props.buttonAfter && this.props.opt3}
             </label>
           </div>
         </form>
