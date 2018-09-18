87001991eff18b39e54d69887a894311d25f2b00
ingridlarssen <ingrid_al_96@hotmail.com>
2018-09-18 11:03:31 +0200

    cleaned up menuContent.js #53

--- src/components/MenuContent.js
+++ src/components/MenuContent.js
@@ -1,15 +1,15 @@
-
 import React, { Component } from 'react';
-
 import { RadioButton } from '../components/radioButton.js';
 import { MenuHeader } from '../components/MenuHeader.js';
 
+//menu for cellphone/smaller screens
 export class MenuContent extends Component {
-    constructor(props, context) {
+
+  constructor(props, context) {
     super(props, context);
-    this.state = { entered : false };
-
-
+    this.state = {
+      entered : false
+    };
   }
 
   componentDidMount() {
@@ -21,25 +21,20 @@ export class MenuContent extends Component {
   }
 
   updateRadioButtonAttr = (e, type) => {
-    //menucontent
     this.props.updateRadioButtonAttr(e, type);
   }
 
-
   render () {
-
-    //menu for mobil
-  return (
-          <div className="menuContent">
-          <MenuHeader menuHeaderTitle="Picture" id="topMenuHeader"/>
-            <RadioButton opt1="Instruments" opt2="Animals" opt3="Childrens Shows" type="selectedPic" updateRadioButtonAttr={this.updateRadioButtonAttr}/>
-          <MenuHeader menuHeaderTitle="Text"/>
-            <RadioButton opt1="Instruments" opt2="Animals" opt3="Childrens Shows" type="selectedText" updateRadioButtonAttr={this.updateRadioButtonAttr}/>
-          <MenuHeader menuHeaderTitle="Audio"/>
-            <RadioButton opt1="Instruments" opt2="Animals" opt3="Childrens Shows" type="selectedAudio" updateRadioButtonAttr={this.updateRadioButtonAttr}/>
-          </div>
-
-
+    return (
+      <div className="menuContent">
+        <MenuHeader menuHeaderTitle="Picture" id="topMenuHeader"/>
+          <RadioButton opt1="Instruments" opt2="Animals" opt3="Childrens Shows" type="selectedPic" updateRadioButtonAttr={this.updateRadioButtonAttr}/>
+        <MenuHeader menuHeaderTitle="Text"/>
+          <RadioButton opt1="Instruments" opt2="Animals" opt3="Childrens Shows" type="selectedText" updateRadioButtonAttr={this.updateRadioButtonAttr}/>
+        <MenuHeader menuHeaderTitle="Audio"/>
+          <RadioButton opt1="Instruments" opt2="Animals" opt3="Childrens Shows" type="selectedAudio" updateRadioButtonAttr={this.updateRadioButtonAttr}/>
+      </div>
     );
   }
+
 }
